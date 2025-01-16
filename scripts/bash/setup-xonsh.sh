984421a0292864599cbcd664f6a811a098bf02fd
Matheus Castello <matheus.castello@toradex.com>
2025-01-16 02:12:28 -0300

    scripts: setup-xonsh: Make the add to path conditional
    This was adding trash to the .bashrc and .xonshrc files every time the
    script was executed. Now it checks if the line is already present before
    adding it.
    
    Signed-off-by: Matheus Castello <matheus.castello@toradex.com>

--- scripts/bash/setup-xonsh.sh
+++ scripts/bash/setup-xonsh.sh
@@ -2,6 +2,13 @@
 
 echo "🐚 SETUP XONSH"
 
+# check if the xonsh is on the path if not install it
+if command -v xonsh &> /dev/null; then
+    echo "xonsh is already installed"
+    echo "all ok ✅"
+    exit 0
+fi
+
 pipx install xonsh
 pipx inject xonsh distro
 pipx inject xonsh shtab
@@ -13,7 +20,12 @@ pipx inject xonsh xontrib-powerline2
 pipx inject xonsh python-lsp-server
 pipx inject xonsh pylsp-rope
 
-# add xonsh to the path
-echo "export PATH=\$PATH:\$HOME/.local/bin" >> ~/.bashrc
+# add xonsh to the path if not already present
+if ! grep -q "export PATH=\$PATH:\$HOME/.local/bin" ~/.bashrc; then
+    echo "export PATH=\$PATH:\$HOME/.local/bin" >> ~/.bashrc
+fi
+
 # also for .xonshrc itself
-echo "\$PATH.insert(0, '$HOME/.local/bin')" >> ~/.xonshrc
+if ! grep -q "\$HOME/.local/bin" ~/.xonshrc; then
+    echo "\$PATH.insert(0, '$HOME/.local/bin')" >> ~/.xonshrc
+fi
